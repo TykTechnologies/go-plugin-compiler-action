1bbb0a952840392d46d993b647949c4e0ac71afd
Alok G Singh <alephnull@hcoop.net>
2020-12-12 20:39:37 +0530

    Actually fetch the gw code

--- build.sh
+++ build.sh
@@ -10,11 +10,14 @@ if [[ -z $plugin_name ]]; then
     exit 1
 fi
 
+curl -sL "https://api.github.com/repos/TykTechnologies/tyk/tarball/${INPUT_GW_TAG}" | \
+    tar -C $TYK_GW_PATH --strip-components=1 -xzf - 
+yes | cp -r $TYK_GW_PATH/vendor/* $GOPATH/src && rm -rf $TYK_GW_PATH/vendor
+
 yes | cp -r * $PLUGIN_BUILD_PATH
 yes | cp -r vendor $GOPATH/src || true \
 rm -rf $PLUGIN_BUILD_PATH/vendor
 
-pwd=$(pwd)
 cd $PLUGIN_BUILD_PATH \
     && go build -buildmode=plugin -ldflags "-pluginpath=$plugin_path" -o $plugin_name \
     && mv $plugin_name $pwd
