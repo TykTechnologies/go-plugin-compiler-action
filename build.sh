321fae979533066ccb42154b6e5655917bc53189
Alok G Singh <alephnull@hcoop.net>
2020-12-12 21:09:04 +0530

    Move plugin back to starting dir

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ if [[ -z $plugin_name ]]; then
     exit 1
 fi
 
+opwd=$(pwd)
+
 curl -sL "https://api.github.com/repos/TykTechnologies/tyk/tarball/${INPUT_GW_TAG}" | \
     tar -C $TYK_GW_PATH --strip-components=1 -xzf - 
 yes | cp -r $TYK_GW_PATH/vendor/* $GOPATH/src && rm -rf $TYK_GW_PATH/vendor
@@ -20,4 +22,4 @@ rm -rf $PLUGIN_BUILD_PATH/vendor
 
 cd $PLUGIN_BUILD_PATH \
     && go build -buildmode=plugin -ldflags "-pluginpath=$plugin_path" -o $plugin_name \
-    && mv $plugin_name $pwd
+    && mv $plugin_name $opwd
